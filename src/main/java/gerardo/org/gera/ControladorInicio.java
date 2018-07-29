7e71fc78569cfea98b87bb91c6672702e5e7ddbe
Juan Carlos Campos <rapidclimate@gmail.com>
2018-07-29 13:00:37 -0500

    Initial commit

---
+++ src/main/java/gerardo/org/gera/ControladorInicio.java
@@ -0,0 +1,18 @@
+package gerardo.org.gera;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ControladorInicio {
+    @RequestMapping("/hola")
+    public String metodo1(){
+        float edad=30;
+        int dias=365;
+        float edadendias=edad*dias;
+        return "mi edad en dias es "+edadendias;
+    }
+
+
+}
---
+++ src/main/java/gerardo/org/gera/GeraApplication.java
@@ -0,0 +1,12 @@
+package gerardo.org.gera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeraApplication.class, args);
+	}
+}
